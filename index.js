e8737475db4a4bbba16e9bc0f84b868450e65c5d
RajasiDeshpande <rajasideshpande@gmail.com>
2024-06-04 22:25:32 +0530

    index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let app=express();
 let port=8080;
 let path=require("path");
 const Chat=require("./models/chat.js");
+const methodOverride=require("method-override");
 app.listen(port,()=>{
     console.log(`app is listening through ${port}`);
 })
@@ -11,6 +12,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
+app.use(methodOverride("_method"));
 const mongoose=require("mongoose");
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
@@ -44,5 +46,18 @@ app.post("/chats",(req,res)=>{
     .catch((err)=>{
         console.log("some error occured");
     })
+    res.redirect("/chats");
+})
+//Edit route
+app.get("/chats/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    let chat=await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+})
+//Update route
+app.put("/chats/:id",async(req,res)=>{
+    let {id}=req.params;
+    let {msg:newMsg}=req.body;
+    let updatedChat=await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true,new:true});
     res.redirect("/chats");
 })
